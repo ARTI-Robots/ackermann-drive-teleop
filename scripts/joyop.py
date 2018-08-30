00031def412dba676d040c5a5dc43eea7350e3c9
Michael Stradner <michael.stradner@student.tugraz.at>
2018-08-30 15:50:59 +0200

    Disabled console output for joystick-node

--- scripts/joyop.py
+++ scripts/joyop.py
@@ -27,9 +27,9 @@ class AckermannDriveJoyop:
             self.max_steering_angle = float(args[1])
             cmd_topic = '/' + args[2]
         else:
-            self.max_speed = 0.2
+            self.max_speed = 1.0
             self.max_steering_angle = 0.7
-            cmd_topic = 'ackermann_cmd'
+            cmd_topic = 'cmd_ackermann'
 
         self.speed = 0
         self.steering_angle = 0
@@ -37,10 +37,10 @@ class AckermannDriveJoyop:
         self.drive_pub = rospy.Publisher(cmd_topic, AckermannDrive,
                                          queue_size=1)
         rospy.Timer(rospy.Duration(1.0/5.0), self.pub_callback, oneshot=False)
-        rospy.loginfo('ackermann_drive_joyop_node initialized')
+        #rospy.loginfo('ackermann_drive_joyop_node initialized')
 
     def joy_callback(self, joy_msg):
-        self.speed = joy_msg.axes[2] * self.max_speed;
+        self.speed = joy_msg.axes[1] * self.max_speed;
         self.steering_angle = joy_msg.axes[3] * self.max_steering_angle;
 
 
@@ -49,17 +49,17 @@ class AckermannDriveJoyop:
         ackermann_cmd_msg.speed = self.speed
         ackermann_cmd_msg.steering_angle = self.steering_angle
         self.drive_pub.publish(ackermann_cmd_msg)
-        self.print_state()
+        #self.print_state()
 
-    def print_state(self):
-        sys.stderr.write('\x1b[2J\x1b[H')
-        rospy.loginfo('\x1b[1M\r'
-                      '\033[34;1mSpeed: \033[32;1m%0.2f m/s, '
-                      '\033[34;1mSteering Angle: \033[32;1m%0.2f rad\033[0m',
-                      self.speed, self.steering_angle)
+    #def print_state(self):
+        #sys.stderr.write('\x1b[2J\x1b[H')
+        #rospy.loginfo('\x1b[1M\r'
+        #              '\033[34;1mSpeed: \033[32;1m%0.2f m/s, '
+        #              '\033[34;1mSteering Angle: \033[32;1m%0.2f rad\033[0m',
+        #              self.speed, self.steering_angle)
 
     def finalize(self):
-        rospy.loginfo('Halting motors, aligning wheels and exiting...')
+        #rospy.loginfo('Halting motors, aligning wheels and exiting...')
         ackermann_cmd_msg = AckermannDrive()
         ackermann_cmd_msg.speed = 0
         ackermann_cmd_msg.steering_angle = 0
